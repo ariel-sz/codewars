9b3c4f1e6901d474cfb275aa16ef09918984d969
ariel.sazu <ariel.sazu@gmail.com>
2025-06-12 10:22:27 -0400

    2025-06-12 daily challenges

---
+++ 8 kyu/Convert_a_Boolean_to_a_String/main.js
@@ -0,0 +1,11 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+function booleanToString(b){
+  if (b == true) {
+    return b = "true"
+  }
+  else return b = "false"
+}
